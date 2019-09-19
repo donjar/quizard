58d751060951f0ecaee6e0f19fd1693fde97ba51
Caryn Heng <heng.zhixuan.caryn@dhs.sg>
2019-09-19 12:01:20 +0800

    add lazy loading routes (#34)

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,23 +1,26 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import HomeContainer from './components/containers/home/HomeContainer';
-import LoginContainer from './components/containers/login/LoginContainer';
-import QuizCompleteContainer from './components/containers/quiz-complete/QuizCompleteContainer';
-import QuizCreateContainer from './components/containers/quiz-create/QuizCreateContainer';
-import QuizQuestionContainer from './components/containers/quiz-question/QuizQuestionContainer';
-import QuizStartContainer from './components/containers/quiz-start/QuizStartContainer';
+
+const HomeContainer = lazy(() => import('./components/containers/home/HomeContainer'));
+const LoginContainer = lazy(() => import('./components/containers/login/LoginContainer'));
+const QuizCompleteContainer = lazy(() => import('./components/containers/quiz-complete/QuizCompleteContainer'));
+const QuizCreateContainer = lazy(() => import('./components/containers/quiz-create/QuizCreateContainer'));
+const QuizQuestionContainer = lazy(() => import('./components/containers/quiz-question/QuizQuestionContainer'));
+const QuizStartContainer = lazy(() => import('./components/containers/quiz-start/QuizStartContainer'));
 
 const App: React.FC = () => {
   return (
     <>
       <Router>
-        <Route path="/" exact={true} component={LoginContainer} />
-        <Route path="/home" component={HomeContainer} />
-        <Route path="/login" component={LoginContainer} />
-        <Route path="/quiz" component={QuizQuestionContainer} />
-        <Route path="/quiz-create" component={QuizCreateContainer} />
-        <Route path="/quiz-complete" component={QuizCompleteContainer} />
-        <Route path="/quiz-start" component={QuizStartContainer} />
+        <Suspense fallback={<div>Loading...</div>}>
+          <Route path="/" exact={true} component={LoginContainer} />
+          <Route path="/home" component={HomeContainer} />
+          <Route path="/login" component={LoginContainer} />
+          <Route path="/quiz" component={QuizQuestionContainer} />
+          <Route path="/quiz-create" component={QuizCreateContainer} />
+          <Route path="/quiz-complete" component={QuizCompleteContainer} />
+          <Route path="/quiz-start" component={QuizStartContainer} />
+        </Suspense>
       </Router>
     </>
   );

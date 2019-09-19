27fefb2ce02d6562cbf06c63b41ed9fbf53eff5e
Herbert Ilhan Tanujaya <mail@herbert.id>
2019-09-19 09:25:13 +0800

    Update API url (#32)

--- web/src/api.ts
+++ web/src/api.ts
@@ -1,4 +1,4 @@
-const apiUrl = 'http://localhost:8000';
+const apiUrl = 'http://165.22.54.169';
 
 export const login = async (email: string, password: string) => {
   const res = await fetch(`${apiUrl}/login`, {

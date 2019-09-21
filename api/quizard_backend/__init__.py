3131b82037354c3987c345acfbd8fa179b8b0375
Son Nguyen Thanh <thanhson16198@gmail.com>
2019-09-21 15:16:17 +0800

    Increase access token's expiry to 48 hours (#52)

--- api/quizard_backend/__init__.py
+++ api/quizard_backend/__init__.py
@@ -15,7 +15,6 @@ app.config["JWT_SECRET_KEY"] = "oi123n1k231kloiqwescqklwn"  # Remove later
 app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(minutes=15)
 
 app.config["CORS_AUTOMATIC_OPTIONS"] = True
-CORS(app)
 
 # Note: Gino doesn't auto-generate any new changes in the schema
 # Use alembic to apply new changes to the db
@@ -26,6 +25,7 @@ db = Gino()
 # to avoid circular importing
 db.init_app(app)
 JWTManager(app)
+CORS(app)
 
 
 async def create_tables(app, loop):

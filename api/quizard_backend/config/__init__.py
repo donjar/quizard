7c68803ccfab3df59dc9fbcb78e5769428886e88
Son <son@fr0st.local>
2019-09-22 23:47:04 +0700

    Add SLS to back-end

--- api/quizard_backend/config/__init__.py
+++ api/quizard_backend/config/__init__.py
@@ -1,14 +1,14 @@
 from os import environ
 from quizard_backend.config.db import DB_CONFIG
 
-mode = environ.get("MODE", "development").lower()
+MODE = environ.get("MODE", "development").lower()
 
 SANIC_CONFIG = {**DB_CONFIG, "KEEP_ALIVE_TIMEOUT": 10}
 
 SANIC_RUN_CONFIG = {
     "host": "0.0.0.0",
     "port": 8000,
-    "debug": mode != "production",
-    "access_log": mode != "production",
+    "debug": MODE != "production",
+    "access_log": MODE != "production",
     "workers": 3,
 }

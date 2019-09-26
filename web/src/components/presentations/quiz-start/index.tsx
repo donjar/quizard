a37943b5f28fd5c01fd4686b98a3b541fcd1f53e
Alistair <ongalistair@gmail.com>
2019-09-26 17:52:09 +0800

    Add margins and quiz type

--- web/src/components/presentations/quiz-start/index.tsx
+++ web/src/components/presentations/quiz-start/index.tsx
@@ -6,6 +6,17 @@ import { BodyAfterNavBarWithPadding } from '../common/BodyAfterNavBarWithPadding
 import { StartButton } from '../common/buttons/StartButton';
 import { CenteredDiv } from '../common/CenteredDiv';
 import { QuizStartNavBar } from './QuizStartNavBar';
+import styled from 'styled-components';
+
+const BoldedText = styled.span`
+  font-weight: bold;
+`;
+
+const StyledBody = styled(BodyAfterNavBarWithPadding)`
+  * {
+    margin: 20px 0;
+  }
+`;
 
 const QuizStart: React.FC<IQuizStartProps> = ({ quiz, isNewQuiz, onStartClick }) => {
   const { name, description } = quiz;
@@ -13,9 +24,13 @@ const QuizStart: React.FC<IQuizStartProps> = ({ quiz, isNewQuiz, onStartClick })
   return (
     <BeigeBackground>
       <QuizStartNavBar />
-      <BodyAfterNavBarWithPadding>
-        <div>{name}</div>
+      <StyledBody>
+        <h1>{name}</h1>
         <div>{description}</div>
+        <div>
+          <BoldedText>Quiz rules:</BoldedText> MCQ format, first option you
+          select will be recorded as your answer
+        </div>
         <CenteredDiv>
           {isNewQuiz ? (
             <StartButton onClick={onStartClick}>Start</StartButton>
@@ -23,7 +38,7 @@ const QuizStart: React.FC<IQuizStartProps> = ({ quiz, isNewQuiz, onStartClick })
             <StartButton onClick={onStartClick}>Continue</StartButton>
           )}
         </CenteredDiv>
-      </BodyAfterNavBarWithPadding>
+      </StyledBody>
     </BeigeBackground>
   );
 };

74e87b7c8289f5c5381d2db8d380a7f2c61d11b7
Alistair Ong <ongalistair@gmail.com>
2019-09-27 12:03:28 +0800

    Remove redux-devtools (#150)

--- web/src/store/store.ts
+++ web/src/store/store.ts
@@ -4,14 +4,15 @@ import rootReducer from './rootReducer';
 
 export type AppState = ReturnType<typeof rootReducer>;
 
-declare global {
-  // tslint:disable-next-line: interface-name
-  interface Window {
-    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
-  }
-}
+// declare global {
+//   // tslint:disable-next-line: interface-name
+//   interface Window {
+//     __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+//   }
+// }
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers = compose;
 
 export function configureStore(initialState = {}) {
   return createStore(

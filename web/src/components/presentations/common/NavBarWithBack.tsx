6eff02a3c0cab032d3b96d9176cdac7ff1cc69e7
hzxcaryn <heng.zhixuan.caryn@dhs.sg>
2019-09-27 20:30:44 +0800

    fix lint

--- web/src/components/presentations/common/NavBarWithBack.tsx
+++ web/src/components/presentations/common/NavBarWithBack.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { ChevronLeft, Home } from 'react-feather';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 import { ReactComponent as Quizard } from '../../../svg/quizard.svg';
 import NakedButton from './buttons/NakedButton';
 import NavBar from './NavBar';
-import styled from 'styled-components';
 
 const EmptyBox = styled.div`
   width: 72px;

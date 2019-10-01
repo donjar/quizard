c61ebd9cc2e8843cbc9bf94a8bd9d3bfbbadcb17
hzxcaryn <heng.zhixuan.caryn@dhs.sg>
2019-10-01 19:54:35 +0800

    use immer for quiz-create so initial state is not mutated

--- web/src/components/containers/quiz-create/redux/reducers.ts
+++ web/src/components/containers/quiz-create/redux/reducers.ts
@@ -1,3 +1,4 @@
+import produce from 'immer';
 import { IQuizCreateState } from '../../../../interfaces/quiz-create';
 import initialState from './initialState';
 import {
@@ -22,75 +23,83 @@ export default function quizCreateReducer(
   state = initialState,
   action: IQuizCreateActionTypes
 ): IQuizCreateState {
-  const newQuestions = [...state.questions];
-
   switch (action.type) {
     case ADD_QUESTION:
-      return {
-        ...state,
-        questions: [...state.questions, action.payload]
-      };
-    case DELETE_QUESTION:
-      newQuestions.splice(action.payload, 1);
-      return {
-        ...state,
-        questions: newQuestions
-      };
-    case ADD_ANSWER_OPTION:
-      newQuestions[action.payload].options.push('');
-      return {
-        ...state,
-        questions: newQuestions
-      };
-    case DELETE_ANSWER_OPTION:
-      const newQuestion = { ...state.questions[action.payload.questionIdx] };
-      newQuestion.options.splice(action.payload.optionIdx, 1);
-      if (
-        newQuestion.options.length > 0 &&
-        newQuestion.correctOption >= newQuestion.options.length
-      ) {
-        newQuestion.correctOption = newQuestion.options.length - 1;
-      }
+      return produce(state, (draft) => {
+        draft.questions.push(action.payload);
+      });
 
-      newQuestions[action.payload.questionIdx] = newQuestion;
-      return { ...state, questions: newQuestions };
+    case DELETE_QUESTION:
+      return produce(state, (draft) => {
+        draft.questions.splice(action.payload, 1);
+      });
+
+    case ADD_ANSWER_OPTION:
+      return produce(state, (draft) => {
+        draft.questions[action.payload].options.push('');
+      });
+
+    case DELETE_ANSWER_OPTION:
+      return produce(state, (draft) => {
+        const newQuestion = draft.questions[action.payload.questionIdx];
+        newQuestion
+          .options
+          .splice(action.payload.optionIdx, 1);
+        if (
+          newQuestion.options.length > 0 &&
+          newQuestion.correctOption >= newQuestion.options.length
+        ) {
+          newQuestion.correctOption = newQuestion.options.length - 1;
+        }
+      });
 
     case CHANGE_ANSWER_OPTION:
-      newQuestions[action.payload.questionIdx].options[
-        action.payload.optionIdx
-      ] = action.payload.newAnswerOption;
-      return { ...state, questions: newQuestions };
+      return produce(state, (draft) => {
+        draft
+          .questions[action.payload.questionIdx]
+          .options[
+            action.payload.optionIdx
+          ] = action.payload.newAnswerOption;
+      });
 
     case SET_CORRECT_ANSWER:
-      newQuestions[action.payload.questionIdx].correctOption =
-        action.payload.optionIdx;
-      return { ...state, questions: newQuestions };
+      return produce(state, (draft) => {
+        draft
+          .questions[action.payload.questionIdx]
+          .correctOption = action.payload.optionIdx;
+      });
 
     case CHANGE_NAME:
-      return { ...state, name: action.payload };
+      return produce(state, (draft) => {
+        draft.name = action.payload;
+      });
 
     case CHANGE_DESCRIPTION:
-        return { ...state, description: action.payload };
+      return produce(state, (draft) => {
+        draft.description = action.payload;
+      });
 
     case CHANGE_QUESTION_TEXT:
-      newQuestions[action.payload.questionIdx].text =
-        action.payload.newText;
-      return { ...state, questions: newQuestions };
+      return produce(state, (draft) => {
+        draft
+          .questions[action.payload.questionIdx]
+          .text = action.payload.newText;
+      });
 
     case SET_ERROR:
-      return {
-        ...state,
-        error: action.payload,
-      };
+      return produce(state, (draft) => {
+        draft.error = action.payload;
+      });
 
     case QUIZ_CREATED:
-      return {
-        ...state,
-        createdQuizId: action.payload
-      };
+      return produce(state, (draft) => {
+        draft.createdQuizId = action.payload;
+      });
 
     case QUIZ_CREATE_RESET:
-      return initialState;
+      return {
+        ...initialState,
+      };
 
     default:
       return state;
